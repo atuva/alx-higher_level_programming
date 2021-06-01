f365e5187df8ba0cbb3f68378cadbf1b151ffcab
Alfred Tuva <muindetuva@gmail.com>
2021-06-01 13:43:50 +0000

    Add task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,10 @@
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer, True)
+    def test_max(self):
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([1, 3, 2]), 3)
