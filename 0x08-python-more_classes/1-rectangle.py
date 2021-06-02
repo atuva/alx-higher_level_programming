57a40ed949f8fa38232420973072850a688a3273
Alfred Tuva <muindealfred01@gmail.com>
2021-06-02 17:09:29 +0300

    Change from value to width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,14 +15,14 @@ class Rectangle:
         '''
         if not isinstance(width, int):
             raise TypeError('width must be an integer')
-        elif value < 0:
+        elif width < 0:
             raise ValueError('width must be >= 0')
         else:
             self.__width = width
 
         if not isinstance(height, int):
             raise TypeError('height must be an integer')
-        elif value < 0:
+        elif height < 0:
             raise ValueError('height must be >= 0')
         else:
             self.__height = height
