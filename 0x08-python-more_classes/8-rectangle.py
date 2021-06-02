eb0d2e83bf8443982b0b59af7d52522a48b5d527
Alfred Tuva <muindealfred01@gmail.com>
2021-06-02 18:17:05 +0300

    Add docstring for the static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -113,6 +113,9 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        '''
+        Returns the bigger of the two rectangles
+        '''
         if not isinstance(rect_1, Rectangle):
             raise TypeError('rect_1 must be an instance of Rectangle')
         elif not isinstance(rect_2, Rectangle):
