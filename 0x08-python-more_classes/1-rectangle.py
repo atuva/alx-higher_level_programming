0fe8b40bdc36418865ac84be09e0f0d745f102b7
Alfred Tuva <muindealfred01@gmail.com>
2021-06-02 17:07:15 +0300

    Add checker in init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,20 @@ class Rectangle:
             width (int): The width of the rectangle
             height (int): The height of the rectangle
         '''
-        self.__width = width
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = height
+
 
     @property
     def width(self):
