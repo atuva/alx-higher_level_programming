8811e64d374446636050f4441cbe31b70bf68b7c
Alfred Tuva <muindealfred01@gmail.com>
2021-06-04 15:14:16 +0300

    Fix pep8 error

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -11,7 +11,7 @@ def is_kind_of_class(obj, a_class):
         obj: The object to check the instance for
         a_class: The class to compare the object with
     Returns:
-        boolean: True if the object is exactly an instance of the specified class ;
+        boolean: True if object is exactly an instance of the specified class
     '''
     if isinstance(obj, a_class):
         return True
