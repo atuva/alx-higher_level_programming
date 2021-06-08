45d448ad0e716c8a832c1519ff4226c064151df9
Alfred Tuva <muindealfred01@gmail.com>
2021-06-08 19:02:26 +0300

    Add more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,11 +6,17 @@ import unittest
 from models.base import Base
 
 class TestBaseClass(unittest.TestCase):
+    def setUp(self):
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base(12)
+
+    def tearDown(self):
+        del self.b1
+        del self.b2
+        del self.b3
+
     def test_id(self):
-        b1 = Base()
-        b2 = Base()
-        b3 = Base(12)
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 12)
-        
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 2)
+        self.assertEqual(self.b3.id, 12)
