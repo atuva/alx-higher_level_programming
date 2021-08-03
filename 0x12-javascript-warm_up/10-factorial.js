b788a1d2817b4a753fb4ce22fe1bbe29625147db
Alfred Tuva <muindetuva@gmail.com>
2021-08-03 22:19:21 +0300

    Add missing semicolon and make num variable a const

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@ const argv = require('process').argv;
 
 function factorial (num) {
   if (num === 0 || isNaN(num)) {
-    return 1
+    return 1;
   }
   return num * factorial(num - 1);
 }
 
-let num = Number(argv[2]);
+const num = Number(argv[2]);
 console.log(factorial(num));
