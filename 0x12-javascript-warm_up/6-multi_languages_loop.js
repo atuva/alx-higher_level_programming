7c853c8b04c0a2223b2e9e561ccf95d6ad3e0a17
Alfred Tuva <muindetuva@gmail.com>
2021-08-03 22:16:32 +0300

    Capitalise 'S' in 'Javascript'

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const langs = ['C', 'Python', 'Javascript'];
+const langs = ['C', 'Python', 'JavaScript'];
 const attrs = ['fun', 'cool', 'amazing'];
 for (let i = 0; i < langs.length; i++) {
   console.log(langs[i] + ' is ' + attrs[i]);
