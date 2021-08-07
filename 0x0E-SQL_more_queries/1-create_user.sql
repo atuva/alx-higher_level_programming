30be2738be876d702cc0864c02f1bf3bceb66b88
Alfred Tuva <muindetuva@gmail.com>
2021-08-07 09:13:11 +0000

    cpy

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
--- creates the MySQL server user user_0d_1.
--- password user_0d_1_pwd and grants privileges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- creates user user_0d_1 with password user_0d_1_pwd
+-- with all privileges.
+-- does not fail if user exists
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+ON *.*
+TO user_0d_1@localhost
