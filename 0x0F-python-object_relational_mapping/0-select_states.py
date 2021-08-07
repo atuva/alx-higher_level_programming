53cade368ba4d02088d6eb824bccf9fc3dadfd64
Alfred Tuva <muindetuva@gmail.com>
2021-08-07 18:01:05 +0000

    Add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Contains a MySQLdb script that performs read operations on a database
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    '''Selects records from a database'''
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect("localhost", username, password, db_name)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states")
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    db.close()
