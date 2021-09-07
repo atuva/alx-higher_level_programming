281b2d4f607057da7a729a30e1b4caee4e33043a
Alfred Tuva <muindetuva@gmail.com>
2021-09-07 16:42:31 +0300

    Fix typo

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl "$1" -s -X POST -H "Content-Type: appliction/json" -d @"$2"
+curl "$1" -s -X POST -H "Content-Type: application/json" -d @"$2"
