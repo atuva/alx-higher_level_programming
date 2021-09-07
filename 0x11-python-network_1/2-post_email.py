218a33cbbdad50fc826c508b0c3d1ad4da489e4b
Alfred Tuva <muindetuva@gmail.com>
2021-09-07 22:23:50 +0300

    Add documentation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+Module contains a script for sending a post request with an email
+'''
 import urllib.parse
 import urllib.request
 import sys
@@ -13,4 +16,4 @@ if __name__ == '__main__':
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode())
+        print(response.read().decode('utf-8'))
