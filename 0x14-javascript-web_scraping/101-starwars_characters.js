315c37d9b97edba3edc15f0c7e88966ff3b4c455
Alfred Tuva <muindetuva@gmail.com>
2021-09-14 16:15:18 +0000

    Use recursive function

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,6 +5,24 @@ const request = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
 
+// Request module is async and for us to have the list in order we need to
+// make our requests in a synchronous manner
+// Recursive functions to make synchronous requests
+function makeRequests (urls) {
+  const url = urls.shift();
+  request(url, (error, response, body) => {
+    // If the list is not empty do some recursion
+    if (urls.length) {
+      makeRequests(urls);
+    }
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(JSON.parse(body).name);
+  });
+}
+
 // Request the film data
 request(url, (error, response, body) => {
   if (error) {
@@ -13,14 +31,5 @@ request(url, (error, response, body) => {
 
   const characters = JSON.parse(body).characters;
   // Make a request for each character
-  for (const x in characters) {
-    request(characters[x], (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
-
-      const name = JSON.parse(body).name;
-      console.log(name);
-    });
-  }
+  makeRequests(characters);
 });
